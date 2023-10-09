07e25f8cd92fb0275c2cd14e24654db1be74855e
wxl <1695607239@qq.com>
2023-10-09 14:11:26 +0800

    初始化

---
+++ src/main/java/org/wxl/cqiemutualselection/CqieMutualSelectionApplication.java
@@ -0,0 +1,13 @@
+package org.wxl.cqiemutualselection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqieMutualSelectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqieMutualSelectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wxl/cqiemutualselection/CqieMutualSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wxl.cqiemutualselection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqieMutualSelectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
